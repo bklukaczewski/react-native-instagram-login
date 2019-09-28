3520ac57900ebf9ea27bacf1d7ef5e8839a353ad
bklukaczewski <bklukaczewski@gmail.com>
2019-09-28 16:41:13 +0200

    Update TS annotations; Bump version to 1.1.5.

--- index.d.ts
+++ index.d.ts
@@ -3,16 +3,21 @@ declare module 'react-native-instagram-login' {
   import { ViewStyle } from 'react-native';
 
   export interface InstagramLoginProps {
-    clientId: string,
-    redirectUrl: string,
-    scopes?: string[],
-    onLoginSuccess?: (token: string) => void,
-    modalVisible?: boolean,
-    onLoginFailure?: (error: any) => void,
-    responseType?: string,
-    containerStyle?: ViewStyle,
-    wrapperStyle?: ViewStyle,
-    closeStyle?: ViewStyle,
+    clientId: string
+    redirectUrl: string
+    scopes?: string[]
+    onLoginSuccess?: (token: string) => void
+    modalVisible?: boolean
+    onLoginFailure?: (error: any) => void
+    responseType?: string
+    cacheEnabled?: boolean
+    incognito?: boolean
+    thirdPartyCookiesEnabled?: boolean
+    sharedCookiesEnabled?: boolean
+    domStorageEnabled?: boolean
+    containerStyle?: ViewStyle
+    wrapperStyle?: ViewStyle
+    closeStyle?: ViewStyle
   }
 
   export default class InstagramLogin extends React.Component<InstagramLoginProps> {
